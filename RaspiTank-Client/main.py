f15c33d44ddab7b130480cdd165fbb3123a5aaf9
Daniel <rzor1001@gmail.com>
2018-03-27 19:26:26 +0200

    init

--- RaspiTank-Client/main.py
+++ RaspiTank-Client/main.py
@@ -10,13 +10,29 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.factory import Factory
 from kivy.clock import Clock
-from functools import partial
+import socket 
 
 Config.set('graphics', 'width', '800')
 Config.set('graphics', 'height', '480')
 
+def tcp_send(data):
+    TCP_IP = '192.168.0.115'
+    TCP_PORT = 5005
+    BUFFER_SIZE = 1024
+    MESSAGE = data
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    s.send(MESSAGE)
+    data = s.recv(BUFFER_SIZE)
+    s.close()
+
 class MainScreen(Screen):
-    pass
+    def button_1(self):
+        tcp_send('on')
+
+    def button_2(self):
+        tcp_send('off')
 
 class SecondScreen(Screen):
     pass
